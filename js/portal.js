5b7dd1c14ac70a9c9d39a96dcbea3e238e04da35
Ariel Young <arielyoung@Ariels-Air.hsd1.il.comcast.net>
2017-04-17 01:02:33 -0500

    added main landing page, condensed navi bar, and working on payment

--- js/portal.js
+++ js/portal.js
@@ -1,13 +1,30 @@
 function load_login() {
   $("#person_name").html(user.firstName);
   var menu_opts = $(".option");
-  window.alert(menu_opts[0]);
-  for (var x=0; x<menu_opts.length; x++) {
-    menu_opts[x].addEventListener("click",
+  var invis = $(".inact");
+  for (var x=0; x<invis.length; x++) {
+    invis[x].addEventListener("click",
       function() {
         window.alert("Feature not implemented yet!");
       }
     );
   }
+  $("#finances").bind('click', function() {
+    window.location.replace('payment.html');
+  });
+
+  $('#exp').bind('click',  function() {
+    var change;
+    if (menu_opts[0].style.display == 'none') {
+      change = 'block';
+    } else {
+      change = 'none';
+    }
+    for (var x=0; x<menu_opts.length; x++) {
+      menu_opts[x].style.display = change;
+    }
+  });
+
 }
+
 load_login();
