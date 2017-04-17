5819fa869e49e664a392136a5319c7a777cab233
Young <ayoung11@hawk.iit.edu>
2017-04-17 18:16:32 -0500

    added inputs for routing numbers etc to pay

--- js/portal.js
+++ js/portal.js
@@ -9,6 +9,34 @@ function load_login() {
       }
     );
   }
+  var amt_due = user.t_cost - user.t_cred;
+  $('#tut-amt').html('$'+ (user.t_cost - user.t_cred));
+  if (amt_due > 0) {
+    $('#tut-amt').css('color', 'red');
+    $('#tut-amt').css('font-weight', 'bold');
+  }
+
+  $('#p_now').bind('click', function() {
+      if ($('#pay_box').css('display') == 'block') {
+        var act = window.confirm('Proceed with payment?');
+        if (act) {
+          window.location.replace('another.html');
+        } else {
+          window.alert("Payment cancelled.");
+        }
+      } else {
+        $('#pay_box').css('display', 'block');
+      }
+  });
+  $('#print_acc').bind('click', function() {
+    var change;
+    if ($('#print_det').css('display') == 'none') {
+      change = 'block';
+    } else {
+      change = 'none';
+    }
+    $('#print_det').css('display', change);
+    });
   $("#finances").bind('click', function() {
     window.location.replace('payment.html');
   });
@@ -27,6 +55,46 @@ function load_login() {
       menu_opts[x].style.display = change;
     }
   });
+
+  //finance expanding menus
+  $('#c_info').bind('click',  function() {
+    var change;
+    var con_men = $("#fin_aid_info").children();
+    if (con_men[0].style.display == 'none') {
+      change = 'block';
+    } else {
+      change = 'none';
+    }
+    for (var x=0; x<con_men.length; x++) {
+      con_men[x].style.display = change;
+    }
+  });
+  $('#c_info').bind('click',  function() {
+    var change;
+    var con_men = $("#fin_aid_info").children();
+    if (con_men[0].style.display == 'none') {
+      change = 'block';
+    } else {
+      change = 'none';
+    }
+    for (var x=0; x<con_men.length; x++) {
+      con_men[x].style.display = change;
+    }
+  });
+
+  $('#c_info').bind('click',  function() {
+    var change;
+    var con_men = $("#fin_aid_info").children();
+    if (con_men[0].style.display == 'none') {
+      change = 'block';
+    } else {
+      change = 'none';
+    }
+    for (var x=0; x<con_men.length; x++) {
+      con_men[x].style.display = change;
+    }
+  });
+
 
 }
 
